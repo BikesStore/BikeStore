c78c70dd0bc07be6884240728e4d605f92ebcfb5
Jacqueline <gomezjacqueline@gmail.com>
2024-11-06 21:20:03 -0600

    Ventana ventas ya modificada, heelp

--- src/main/java/com/mycompany/db/c.java
+++ src/main/java/com/mycompany/db/c.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class c {
     private static final String URL = "jdbc:postgresql://localhost:5432/control";
-    private static final String USER = "admin";
-    private static final String PASSWORD = "root";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "jacque14g05";
 
     public static Connection conectar() {
         Connection conexion = null;
